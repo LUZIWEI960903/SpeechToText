29c6bfc2c5af044a95dd573d2e4bf7cf2147b78e
WilsonWu <wilson.wu@kantar.com>
2019-07-04 14:38:42 +0800

    更改了请求控制

--- server/SpeechToText/Router/views.py
+++ server/SpeechToText/Router/views.py
@@ -1,21 +1,20 @@
 # from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import base64
 from aip import AipSpeech
 # Create your views here.
 
-
+data = str()
+count=0
 @csrf_exempt
 def index(request):
-
+    global count,data
+    count+=1
+    print(count)
     if request.method == "POST":
         print(request.FILES)
         Blob = request.FILES['audioData']
-        # print(type(Blob))
-        # print(Blob.file)
-        # data_base64 = base64.b64encode(Blob.file.read())
-        # print(data_base64)
 
         """ 你的 APPID AK SK """
         APP_ID = '16710665'
@@ -27,10 +26,18 @@ def index(request):
         return_file = client.asr(Blob.file.read(), 'wav', 16000, {
             'dev_pid': 1536,
         })
-        # print(return_file)
-        # if return_file['err_no'] == 0:
-        print(return_file)
         if return_file['err_no']==0:
-            return JsonResponse({'data':return_file['result']})
-        else :
-            return JsonResponse({'data':'error'})
+
+        # global data
+            data=return_file['result']
+            print(data)
+            return JsonResponse(data)
+        else:
+            return HttpResponse('error')
+    else:
+        # global data
+        print(data)
+        if len(data)>0:
+            return JsonResponse(data)
+        return HttpResponse('error')
+    # return HttpResponse('ok')
